86a06d70db4a4b146e71cc9e6ef549db89b67aca
kkowatsch <kowatsch_22@hotmail.com>
2017-02-02 00:04:25 -0500

    5th Commit

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "https://github/kkowatsch/power-test"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incanter "1.5.7"]
-                 [proto-repl "0.3.1"]]
-  :main ^:skip-aot power_test.core
-  :plugins [[lein-midje "3.0.0"]]
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
-             :source-paths ["dev" "src" "test"]
-             :dependencies [[org.clojure/tools.namespace "0.2.11"]]})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [proto-repl "0.3.1"]
+                 [proto-repl-charts "0.3.2"]
+                 [incanter "1.5.7"]]
+
+    :main ^:skip-aot power_test.core
+    :target-path "target/%s"
+    :profiles {:uberjar {:aot :all}})
--- src/power_test/core.clj
+++ src/power_test/core.clj
@@ -1,11 +1,28 @@
 (ns power_test.core
-  (:use (incanter core stats charts io)))
+  (:use (incanter core charts datasets)
+        (incanter core stats charts io)
+        (incanter core charts excel)))
 
-(def data (read-data "http://www.census.gov/population/international/data/idb/informationGateway.php#DOWNLOAD"
-           :header true))
-(view data)
+(with-data ($rollup :mean :count [:population :fertility :mortality :migration])
+  (read-dataset "http://www.census.gov/population/international/data/idb/region.php?N=%20Results%20&T=13&A=separate&RT=0&Y=2017&R=-1&C=CA"
+             :header true)
+ (view $data)
+ (view (bar-chart :years :indicators))
+ :group-by :mortality
+ :legend true)
 
-(column-names data)
+(view (bar-chart ["a" "b" "c" "d" "e"]
+                 [10 20 30 25 20]))
 
-(with-data data
-  (def bar (bar-chart ($ :population) ($ :year))))
+(view (bar-chart ["a" "a" "b" "b" "c" "c"]
+                 [10 20 30 10 40 20]
+                 :legend true
+                 :group-by ["I" "II" "I" "II" "I" "II"]))
+
+(def data (read-dataset "http://www.census.gov/population/international/data/idb/region.php?N=%20Results%20&T=13&A=separate&RT=0&Y=2017&R=-1&C=CA"))
+(def by-year (group-by data 0))
+(view (bar-chart (sel (last by-year) :cols 2)
+                 (sel (last by-year) :cols 1)
+                 :title "Airline Travel in 1960"
+                 :y-label "Passengers"
+                 :x-label "Month"))
