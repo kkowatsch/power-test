1ac34e663ee6b187db3f6600d68e00b60ebd360d
kkowatsch <kowatsch_22@hotmail.com>
2017-02-05 15:29:05 -0500

    6th commit

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [proto-repl "0.3.1"]
                  [proto-repl-charts "0.3.2"]
-                 [incanter "1.5.7"]]
+                 [incanter "1.5.7"]
+                 [ring "1.5.1"]]
 
     :main ^:skip-aot power_test.core
     :target-path "target/%s"
--- src/power_test/core.clj
+++ src/power_test/core.clj
@@ -1,28 +1,90 @@
 (ns power_test.core
-  (:use (incanter core charts datasets)
-        (incanter core stats charts io)
-        (incanter core charts excel)))
+ (:gen-class)
+ (use '(incanter core stats charts io)))
 
-(with-data ($rollup :mean :count [:population :fertility :mortality :migration])
-  (read-dataset "http://www.census.gov/population/international/data/idb/region.php?N=%20Results%20&T=13&A=separate&RT=0&Y=2017&R=-1&C=CA"
-             :header true)
- (view $data)
- (view (bar-chart :years :indicators))
- :group-by :mortality
- :legend true)
+(defn force-layout-plot []
+  (d3-page "Force-Directed Layout"
+           "webviz.force.force_layout();"
+           [:div#force.chart [:svg]]))
 
-(view (bar-chart ["a" "b" "c" "d" "e"]
-                 [10 20 30 25 20]))
+(def data
+  (read-dataset
+    "http://github.com/liebke/incanter/raw/master/data/cars.csv"
+     :header true))
 
-(view (bar-chart ["a" "a" "b" "b" "c" "c"]
-                 [10 20 30 10 40 20]
-                 :legend true
-                 :group-by ["I" "II" "I" "II" "I" "II"]))
+;; view the dimensions of the dataset
+(dim data)
 
-(def data (read-dataset "http://www.census.gov/population/international/data/idb/region.php?N=%20Results%20&T=13&A=separate&RT=0&Y=2017&R=-1&C=CA"))
-(def by-year (group-by data 0))
-(view (bar-chart (sel (last by-year) :cols 2)
-                 (sel (last by-year) :cols 1)
-                 :title "Airline Travel in 1960"
-                 :y-label "Passengers"
-                 :x-label "Month"))
+;; view the column names
+(col-names data)
+
+(with-data (get-dataset :cars)
+  (view (conj-cols (range (nrow $data)) $data)))
+
+;; plot a scatter plot of speed vs. distance and add a regression line
+(with-data data
+  (def lm (linear-model ($ :dist) ($ :speed)))
+  (doto (scatter-plot ($ :speed) ($ :dist))
+    (add-lines ($ :speed) (:fitted lm))
+    view))
+
+
+;; create a new dataset that includes the orig data and the
+;; fitted values from the liner-model function
+(def results (conj-cols data (:fitted lm)))
+
+;; give the new dataset meaningful column names
+(def results (col-names data [:speed :dist :predicted-dist]))
+
+;; do both steps at once with the -> macro, and also add the residuals to the new dataset
+(def results (-> (conj-cols data (:fitted lm) (:residuals lm))
+                 (col-names [:speed :dist :predicted :residuals])))
+
+
+;; now use the $where function
+
+($where {:speed 10} results)
+($where {:speed {:$gt 10 :$lt 20}} results)
+($where {:speed {:$in #{4 7 24 25}}} results)
+($where {:speed {:$nin #{4 7 24 25}}} results)
+
+(with-data results
+  (mean ($ :speed ($where {:residuals {:$gt -10 :$lt 10}}))))
+
+
+(with-data results
+  (conj-rows ($where {:speed {:$lt 10}})
+             ($where {:speed {:$gt 20}})))
+
+(with-data results
+  ($where (fn [row] (or (< (:speed row) 10)
+                     (> (:speed row) 20)))))
+
+
+;; Now let's use MongoDB
+(use 'somnium.congomongo)
+(use 'incanter.mongodb)
+
+;; connect to a MongoDB server running on the localhost on the default port.
+(mongo! :db "mydb")
+
+(insert-dataset :breaking-dists results)
+
+(def breaking-dists (fetch-dataset :breaking-dists))
+(col-names breaking-dists)
+(view breaking-dists)
+
+(insert-dataset :breaking-dists breaking-dists)
+(view (fetch-dataset :breaking-dists))
+
+;; use fetch-dataset's :where option to retrieve only the
+;; rows where the speed is between 10 and 20 mph, and
+;; then calculate the mean breaking distance.
+
+(with-data (fetch-dataset :breaking-dists)
+        :where {:speed {:$gt 10 :$lt 20}}
+  (mean ($ :dist)))
+
+
+
+(doc incanter.mongodb)
